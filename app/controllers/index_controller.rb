659970e77db2d9ef38d3491d16dae48b8b676460
Nicolás Salas V <nikosalas@gmail.com>
2018-11-05 21:42:01 -0300

    Implementar index con jumbotron y dashboard

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,4 +1,6 @@
 class IndexController < ApplicationController
   def hello
+    @personas = Persona.where("puntos > 0").order(puntos: :desc).take(5)
+    @peleas = Pelea.joins(:persona).order(created_at: :desc).take(5)
   end
 end
